e0ad745dc6fc8972b52e43da8ba795c88663bdff
vaishnavi <vaishnavip@prdxn.com>
2020-04-15 16:52:41 +0530

    created array to store source of images in it

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var imgs = [];
+imgs[0] = new Image();
+imgs[0].src= "assets/images/st2.jpg";
+imgs[1] = new Image();
+imgs[1].src= "assets/images/st3.png";
+imgs[2] = new Image();
+imgs[2].src= "assets/images/st4.jpg";
+imgs[3] = new Image();
+imgs[3].src="assets/images/st1.jfif";
+var i =0;
